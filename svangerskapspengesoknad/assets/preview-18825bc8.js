ccda886432107668348addf03d86297055b6a61e
tor-nav <tor.roneid@nav.no>
2023-04-13 11:13:17 +0200

    Updates

--- svangerskapspengesoknad/assets/preview-18825bc8.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const d=[r=>o("div",{id:"app",style:{backgroundColor:"white",padding:"40px"},children:o(r,{})})];export{d as decorators};
-//# sourceMappingURL=preview-18825bc8.js.map
